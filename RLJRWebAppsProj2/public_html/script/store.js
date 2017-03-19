a8e720f74c9dcb314d33e4a8c93ecb0645df2595
MightyPhantom <herewegojes1@gmail.com>
2017-03-19 17:31:20 -0400

    Added JS for drag & drop

---
+++ RLJRWebAppsProj2/public_html/script/store.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(function () {
+    $("#content, #left").sortable({
+        connectWith: ".connectedSortable"
+    }).disableSelection();
+});
+
+$(function onclick() {
+    $('#content > div').each(function (index, element) {
+        if($("div > input").is(":checked")){
+            $(this).appendTo("#left");
+            $(this).remove();
+        }
+    });
+}
+
+
