fc65893ca5fcfc29e0f707336e9e90a15b234691
MightyPhantom <herewegojes1@gmail.com>
2017-03-19 22:09:41 -0400

    Drag and Drop now capable of basic math a 3 year old could do.

--- RLJRWebAppsProj2/public_html/script/store.js
+++ RLJRWebAppsProj2/public_html/script/store.js
@@ -1,21 +1,29 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 $(function () {
     $("#content, #left").sortable({
-        connectWith: ".connectedSortable"
+        connectWith: ".connectedSortable",
+        items: "> div",
+        scroll: false,
+        update: function (event, ui) {
+            var totcost = 0;
+            var num = 0;
+            $("#left > div").each(function (index, element) {
+                var holder = $(this).find("#amount").text();
+                totcost = totcost + parseFloat(holder.replace("$", ""));
+                num = num + 1;
+            });
+            $("#count").text("# items: " + num);
+            $("#cost").text("Total Price: $" + totcost);
+        }
     }).disableSelection();
 });
 
 function addtocart() {
     $('#content > div').each(function (index, element) {
-        if($("div > input").is(":checked")){
+        if ($("div > input").is(":checked")) {
             $(this).appendTo("#left");
+            $(this).children("input").attr("checked", false);
+//            $(this).children("input").attr("disabled", true);
         }
     });
 }
-
 
