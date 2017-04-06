d36868286f3b8b608b5f01f68598180827cb5e78
Richard Lively <richielively@gmail.com>
2017-04-05 20:52:38 -0400

    connects to db

--- PeoplesChoice/tools/dbConnect.php
+++ PeoplesChoice/tools/dbConnect.php
@@ -1,6 +1,6 @@
 <?php
 //connects to database
 //NOTE: for testing, connected to local database
-$db = mysql_connect("localhost", "root", "") or die("Error: unable to connect to database");
-mysql_select_db("test") or die("Error: unable to open test table");
+$db = mysql_connect("james.cedarville.edu", "cs4220", "") or die("Error: unable to connect to database");
+mysql_select_db("cs4220") or die("Error: unable to open test table");
 ?>
