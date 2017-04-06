14442440bb4bacfaa58de3462ca615d286ad5402
Richard Lively <richielively@gmail.com>
2017-04-05 22:21:08 -0400

    Added universal files to handle head and footer across the site

---
+++ PeoplesChoice/head.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>People's Choice Alpha</title>
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <meta name="description" content="Simple homepage for web-apps project"/>
+        <meta name="author" content="Jesse Russell and Rich Lively"/>
+
+        <!-- CSS Sylesheets -->
+
+        <!-- JQuery -->
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        
+        <!-- Personal JS -->
+        <script src = "script/addWritein.js"></script>
+        <script src = "script/confirmPass.js"></script>
+    </head>
+    <body>
